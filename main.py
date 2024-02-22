2046eb155a38520f9ae9a3dcfac5b4b179a9d080
Egor <krasnovegor845@mail.ru>
2024-02-22 14:02:58 +0400

    Test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import requests
 
-your_api_key = 'bac449ab-f338-4367-bcd9-28fd15ae6805'
-AUDIO_FILE_PATH = 'https://www.dropbox.com/home/test?preview=Somesh.wav'
+your_api_key = 'bac449ab-f338-4367-bcd9-28fd15ae6805' #API с сайта
 upload_url = 'https://api.fireflies.ai/graphql'
-URL_file = 'https://www.dropbox.com/home/test?preview=Somesh.wav'
-
+URL_file = 'https://www.dropbox.com/home/test?preview=Somesh.wav' #URL версия файла
 
 headers = {
     'Content-Type': 'application/json',
@@ -23,7 +21,6 @@ data = {
         }
     }
 }
-
 
 response = requests.post(upload_url, json = data, headers=headers,)
 print(response.json())
